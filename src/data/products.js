bb2b16629cb2e4bcc4928743ed8c1aaafe452ade
buikhanhlan1410 <buikhanhlan1410@gmail.com>
2026-07-06 14:41:20 +0700

    feat: pivot project to premium room rentals Aura Premium Studios & Rentals

--- src/data/products.js
+++ src/data/products.js
@@ -5,31 +5,31 @@ import floorplanDuplex from '../assets/floorplan_duplex.png';
 
 export const products = [
   {
-    id: '1-bedroom',
+    id: 'cozy-studio',
     image: floorplan1br,
     virtual360Url: 'https://kuula.co/share/collection/7KdBq?logo=1&info=1&fs=1&vr=0&sd=1&thumbs=1',
-    areaNet: '58.4 m²',
-    areaGross: '63.2 m²'
+    areaNet: '28 m²',
+    areaGross: '32 m²'
   },
   {
-    id: '2-bedroom',
+    id: 'loft-suite',
     image: floorplan2br,
     virtual360Url: 'https://kuula.co/share/collection/7PL7K?logo=1&info=1&fs=1&vr=0&sd=1&thumbs=1',
-    areaNet: '88.2 m²',
-    areaGross: '96.5 m²'
+    areaNet: '35 m²',
+    areaGross: '40 m²'
   },
   {
-    id: 'penthouse',
-    image: floorplanPenthouse,
-    virtual360Url: 'https://kuula.co/share/collection/7PryB?logo=1&info=1&fs=1&vr=0&sd=1&thumbs=1',
-    areaNet: '285.5 m²',
-    areaGross: '310.2 m²'
-  },
-  {
-    id: 'duplex',
+    id: 'executive-1br',
     image: floorplanDuplex,
     virtual360Url: 'https://kuula.co/share/collection/7YtK9?logo=1&info=1&fs=1&vr=0&sd=1&thumbs=1',
-    areaNet: '145.8 m²',
-    areaGross: '158.4 m²'
+    areaNet: '48 m²',
+    areaGross: '52 m²'
+  },
+  {
+    id: 'penthouse-studio',
+    image: floorplanPenthouse,
+    virtual360Url: 'https://kuula.co/share/collection/7PryB?logo=1&info=1&fs=1&vr=0&sd=1&thumbs=1',
+    areaNet: '85 m²',
+    areaGross: '95 m²'
   }
 ];
